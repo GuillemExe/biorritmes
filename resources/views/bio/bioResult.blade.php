1d111cf0b23542efbc6b2383020cb062f0404ec6
GuillemExe <guillem.arrojo@mataro.epiaedu.cat>
2020-01-27 16:57:33 +0100

    agregado contenido basico en las vistas y creado el controlador vacio

---
+++ resources/views/bio/bioResult.blade.php
@@ -0,0 +1,20 @@
+@extends('master.master')
+
+@section('pageTitle','Pàgina Index')
+
+
+@section('header')
+<h1>BLADE RESULT</h1>
+@endsection
+
+
+@section('content')
+<h3>Esto es un footer</h3>
+@endsection
+
+
+@section('footer')
+
+<h3>Esto es un footer</h3>
+
+@endsection
