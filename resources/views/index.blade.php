8da03fb060685f183de6a494e6b3120c156c0610
GuillemExe <guillem.arrojo@mataro.epiaedu.cat>
2020-01-27 16:31:04 +0100

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('master.master')
+
+@section('pageTitle','Pàgina Index')
+
+@section('header')
+<h1>Benvingut a l'App de Links</h1>
+@endsection
+
+@section('content')
+<p>content</p>
+@endsection
+
+@section('footer')
+<h3>Això és el footer de App Link</h3>
+@endsection
